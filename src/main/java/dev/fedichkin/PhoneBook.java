93fb0a10c9a1158a85041c16d642496217bf322f
Alekseu F <Tapo4ek@gmail.com>
2024-05-10 17:06:50 +0300

    add implemented

--- src/main/java/dev/fedichkin/PhoneBook.java
+++ src/main/java/dev/fedichkin/PhoneBook.java
@@ -1,7 +1,28 @@
 package dev.fedichkin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-    public int add() {
-        return 0;
+    Map<String, String> phoneBookMap = new HashMap<>();
+
+    public int add(Map<String, String> value) {
+        int count = 0;
+
+        if (phoneBookMap.isEmpty()) {
+            phoneBookMap.putAll(value);
+            count = value.size();
+
+            return count;
+        } else {
+            for (Map.Entry<String, String> entry : value.entrySet()) {
+                if (!phoneBookMap.containsKey(entry.getKey())) {
+                    phoneBookMap.put(entry.getKey(), entry.getValue());
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 }
--- src/test/java/dev/fedichkin/PhoneBookTest.java
+++ src/test/java/dev/fedichkin/PhoneBookTest.java
@@ -3,11 +3,16 @@ package dev.fedichkin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class PhoneBookTest {
 
     @Test
     void testAdd() {
         PhoneBook phoneBook = new PhoneBook();
-        Assertions.assertEquals(-1, phoneBook.add());
+        Map<String, String> map= new HashMap<>();
+        map.put("test", "111111");
+        Assertions.assertEquals(1, phoneBook.add(map));
     }
 }
